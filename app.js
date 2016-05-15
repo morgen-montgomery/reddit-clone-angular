6213610a261dcd429aeac00ede7a3f497b0fe03a
morgen-montgomery <mfett004@gmail.com>
2016-05-14 18:04:13 -0700

    initial controller setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var app = angular.module("RedditClone", []);
+
+app.controller("PostFields", function() {
+  $scope.view = {};
+  $scope.post = [];
+  $scope.view.title = '';
+  $scope.view.author = '';
+  $scope.view.imgTag = '';
+  $scope.view.description = '';
+})
